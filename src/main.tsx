924d8f813e53ccb15c0ea445cddb74e703f5a806
Rysted <sebastianszz@hotmail.com>
2024-02-02 02:23:51 -0300

    Importar React y utilizar React.StrictMode en el componente principal

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
-// import App from "./App";
+import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <>
-    <h1>Hola mundo</h1>
-  </>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
