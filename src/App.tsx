fb677d3d61d38cea8049e4c0578f5cebcfca6b98
Rysted <sebastianszz@hotmail.com>
2024-02-02 02:14:06 -0300

    Comentario: Comenta la importación de Header en App.tsx y elimina su uso en el componente.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import Header from "./components/Layouts/Header";
+// import Header from "./components/Layouts/Header";
 
 const App = () => {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <main className="mt-20 md:mt-0 md:relative md:mr-52 bg-light_background dark:bg-dark_background h-screen">
         <div className="h-52">
           <h1>Hola Mundo</h1>
