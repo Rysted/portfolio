613c59db1fe40bc91d14baaae07be2607a73f83d
Rysted <sebastianszz@hotmail.com>
2024-02-02 02:23:02 -0300

    Agrega título "Hola mundo" a la página principal

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from "react-dom/client";
-import App from "./App";
+// import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <>
+    <h1>Hola mundo</h1>
+  </>
+);
