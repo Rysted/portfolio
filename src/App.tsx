556fcd735b4815d04e4f6d856354b33c43566409
Rysted <sebastianszz@hotmail.com>
2024-02-02 02:20:50 -0300

    Importar el componente Header y eliminar el comentario

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-// import Header from "./components/layouts/Header";
+import Header from "./components/layouts/Header";
 
 const App = () => {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <main className="mt-20 md:mt-0 md:relative md:mr-52 bg-light_background dark:bg-dark_background h-screen">
         <div className="h-52">
           <h1>Hola Mundo</h1>
