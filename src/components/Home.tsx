7a819e97d17a1c6030fb08e4c8cff8028369336a
Rysted <sebastianszz@hotmail.com>
2024-02-06 23:10:22 -0300

    Actualiza estilos de la página de inicio y cambia el título del proyecto de "Portfolio" a "Antiguo portfolio"

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,8 +7,8 @@ const Home = () => {
       id="home"
       className="items-center lg:p-5 pt-24 lg:pt-5 h-screen select-none"
     >
-      <article className="w-full h-full p-5 rounded-xl flex justify-center items-center lg:bg-light_highlight lg:dark:bg-dark_highlight transition-all duration-200 ease-in-out">
-        <div className="max-w-[800px] lg:max-w-[1100px]">
+      <article className="relative overflow-hidden w-full h-full p-5 rounded-xl flex justify-center items-center lg:bg-transparent transition-all duration-200 ease-in-out">
+        <div className="relative max-w-[800px] lg:max-w-[1100px]">
           <h1 className="text-4xl md:text-5xl lg:text-6xl text-light_text dark:text-dark_text transition-all duration-200 ease-in-out">
             ¡Hola! Soy{" "}
             <b className="text-primary-500 dark:text-primary-300 transition-all duration-200 ease-in-out">
@@ -57,6 +57,7 @@ const Home = () => {
             </Redirect>
           </nav>
         </div>
+        <div className="hidden lg:block absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 dark:bg-[radial-gradient(125%_125%_at_50%_10%,#000_40%,#4ab0ff_100%)] bg-[radial-gradient(125%_125%_at_50%_10%,#fff_40%,#4ab0ff_100%)]"></div>
       </article>
     </section>
   );
--- src/components/projects/ProjectData.ts
+++ src/components/projects/ProjectData.ts
@@ -48,7 +48,7 @@ export const ProjectData = [
   },
   {
     image: "portfolioExample.webp",
-    title: "Portfolio",
+    title: "Old portfolio",
     description:
       "Este repositorio contiene el código fuente de mi antiguo portafolio web. Aunque ya no está en uso, decidí mantener este repositorio como referencia y para compartir mi progreso a lo largo del tiempo.",
     tags: ["HTML", "CSS", "JavaScript", "React"],
