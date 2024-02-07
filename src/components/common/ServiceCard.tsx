7274f2bf6c134386c3a7bea15f094148394e6dd0
Rysted <sebastianszz@hotmail.com>
2024-02-07 14:50:56 -0300

    Actualizar estilos de ServiceCard en ServiceCard.tsx

--- src/components/common/ServiceCard.tsx
+++ src/components/common/ServiceCard.tsx
@@ -11,7 +11,7 @@ const ServiceCard = ({
     <div className="p-5 rounded-xl flex flex-col justify-between bg-light_highlight dark:bg-dark_highlight transition-all duration-200 ease-in-out">
       <div className="flex justify-center mb-5">{children}</div>
       <div className="flex flex-col justify-between">
-        <h3 className="text-lg font-bold text-primary-500 dark:text-primary-300 transition-all duration-200 ease-in-out">
+        <h3 className="text-lg font-bold text-primary-600 dark:text-primary-300 transition-all duration-200 ease-in-out">
           {title}
         </h3>
         <p className="text-sm pt-2 text-secondary-700 dark:text-secondary-300 transition-all duration-200 ease-in-out">
