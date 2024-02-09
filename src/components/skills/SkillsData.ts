abcd3bf9fdaf9c075a4a8977f31a19c4acd80962
Rysted <sebastianszz@hotmail.com>
2024-02-09 05:25:38 -0300

    Actualizando SkillsData.ts

--- src/components/skills/SkillsData.ts
+++ src/components/skills/SkillsData.ts
@@ -1,13 +1,21 @@
 export const SkillsData = [
   { title: "Languages", skills: ["Typescript", "Javascript", "HTML", "CSS"] },
-  { title: "Estilización", skills: ["TailwindCSS", "Bootstrap"] },
-  { title: "Frontend", skills: ["React"] },
+  { title: "Frontend", skills: ["React", "TailwindCSS", "Bootstrap"] },
   { title: "Backend", skills: ["Node.js", "Express.js", "EJS", "Sequelize"] },
   { title: "Bases de datos", skills: ["MySQL"] },
   { title: "Diseño", skills: ["Figma", "Miro"] },
   {
     title: "Herramientas",
-    skills: ["Postman", "Prepros", "Jira", "Trello", "Git", "Github"],
+    skills: [
+      "Postman",
+      "phpMyAdmin",
+      "Workbench",
+      "Prepros",
+      "Jira",
+      "Trello",
+      "Git",
+      "Github",
+    ],
   },
   { title: "Idiomas", skills: ["Inglés (lectura)", "Español (nativo)"] },
 ];
